cd2f6867936e4be329f059c746e83e71ddeeed58
VincentPeters <me@vincentpeters.be>
2014-01-10 14:31:08 +0100

    grunt setup, added default files for javascript and sass

---
+++ public/javascript/dev/main.js
@@ -0,0 +1,10 @@
+/*
+*	Pong main javascript file |  https://github.com/contentcowboys/Pong 
+*
+* 	Add your own javascript code in this file
+*/
+
+jQuery(document).ready(function($) 
+{
+	// Your code 
+});
