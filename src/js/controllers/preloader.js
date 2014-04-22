01b586c7b1314960278a5f5d8fe0145259a10b1d
Simon Peters <simonpeters3@gmail.com>
2014-04-22 21:03:50 +0200

    fixed #44

--- src/js/controllers/preloader.js
+++ src/js/controllers/preloader.js
@@ -6,30 +6,57 @@ define(
     	'use strict';
     var preloader = {
     	loaded : 0,
+        reponsive : true,
+        breakPoints : [
+            //FROM LARGE TO SMALL
+            320 , 800 , 1024
+        ],
     	imageArray : [
-    		"http://lorempixel.com/400/203/",
-    		"http://lorempixel.com/400/204/"
+            // PATH , FILENAME, RESPONSIVE
+    		["images/form/","background.png" , true]
     	],
     	preload : function(deferred){
+            var self = this;
     		this.deferred = deferred;
+
     		if(!this.deferred) this.deferred = $.Deferred();
+
     		if(this.imageArray.length === 0){
     			this.deferred.resolve();
     		}else{
-    			var self = this;
-    			this.imageArray.forEach(function(image){
-    					var img = new Image();
-    					img.onload = _.bind(self.imgLoaded, self);
-    					img.onerror = _.bind(self.imgLoaded, self);
-    					img.src = image;
-    			});
+                this.setBreakPoint();
+    			this.imageArray.forEach(self.preloadImage.bind(self));
     		}
     		return this.deferred.promise();
     	},
+        preloadImage : function (image) {
+            var img = new Image();
+            img.onload = this.imgLoaded();
+            img.onerror = this.imgLoaded();
+            if(!image[2]) {
+                img.src = image[0]+image[1];
+            }else{
+                img.src = image[0]+this.breakPoint+"-"+image[1];
+            }
+            
+        },
     	imgLoaded : function(){
     		this.loaded += 1;
     		if(this.loaded === this.imageArray.length) this.deferred.resolve();
-    	}
+    	},
+        setBreakPoint : function () {
+            var self = this,
+                width = $(window).width();
+            var found = this.breakPoints.some(function(breakPoint){
+                if(breakPoint >= width){
+                    self.breakPoint = breakPoint;
+                    found = true;
+                }
+                return (breakPoint >= width);
+
+            });
+            if(!found) this.breakPoint = this.breakPoints[this.breakPoints.length-1];
+        }
     };
     return preloader;
 });
